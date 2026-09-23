a53c42ad9abbc70f1433aaa7ac6bcf1183f799c9
dsferjjkergg <selimochahdoura@gmail.com>
2026-09-23 14:59:10 +0200

    Added Bronze Table creation

---
+++ pipelines/bronze/laps.sql
@@ -0,0 +1,12 @@
+CREATE OR REFRESH STREAMING TABLE motorsport_dev.bronze.laps_raw
+AS
+SELECT
+    *,
+    _metadata.file_path AS source_file,
+    _metadata.file_modification_time AS source_file_modified_at,
+    current_timestamp() AS bronze_ingested_at
+
+FROM STREAM read_files(
+    '/Volumes/motorsport_dev/bronze/raw_fastf1/fastf1/season=*/event=*/session=*/laps/',
+    format => 'parquet'
+);
