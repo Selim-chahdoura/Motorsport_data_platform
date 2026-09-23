2b463730efa738485d2d92e45e378421a08d6d39
dsferjjkergg <selimochahdoura@gmail.com>
2026-09-23 15:59:54 +0200

    Added Weather table creation for bronze

---
+++ pipelines/bronze/weather.sql
@@ -0,0 +1,14 @@
+CREATE OR REFRESH STREAMING TABLE motorsport_dev.bronze.weather_raw
+TBLPROPERTIES (
+    'delta.feature.timestampNtz' = 'supported'
+)
+SELECT
+    *,
+    _metadata.file_path AS source_file,
+    _metadata.file_modification_time AS source_file_modified_at,
+    current_timestamp() AS bronze_ingested_at
+
+FROM STREAM read_files(
+    '/Volumes/motorsport_dev/bronze/raw_fastf1/fastf1/season=*/event=*/session=*/weather/',
+    format => 'parquet'
+); 
