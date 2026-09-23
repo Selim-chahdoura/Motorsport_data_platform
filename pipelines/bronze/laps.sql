03473448d9dc53c5387ea4664e3b96b17e764ca5
dsferjjkergg <selimochahdoura@gmail.com>
2026-09-23 15:46:35 +0200

    Corrected Bronze results table

--- pipelines/bronze/laps.sql
+++ pipelines/bronze/laps.sql
@@ -15,18 +15,3 @@ FROM STREAM read_files(
     '/Volumes/motorsport_dev/bronze/raw_fastf1/fastf1/season=*/event=*/session=*/laps/',
     format => 'parquet'
 );
-
-CREATE OR REFRESH STREAMING TABLE motorsport_dev.bronze.results_raw
-TBLPROPERTIES (
-    'delta.feature.timestampNtz' = 'supported'
-)
-SELECT
-    *,
-    _metadata.file_path AS source_file,
-    _metadata.file_modification_time AS source_file_modified_at,
-    current_timestamp() AS bronze_ingested_at
-
-FROM STREAM read_files(
-    '/Volumes/motorsport_dev/bronze/raw_fastf1/fastf1/season=*/event=*/session=*/results/',
-    format => 'parquet'
-);
